fd1bce48e09b71cbf461d3a6eddc3a7e254af90b
Shiv Keshri <shivprakashkeshri@gmail.com>
2017-08-29 00:50:58 +0530

    new one

--- shivpage/wsgi.py
+++ shivpage/wsgi.py
@@ -9,8 +9,18 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
+from django.conf.urls import url
+
+from django.http import HttpResponse
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shivpage.settings")
 
 application = get_wsgi_application()
+
+
+
+if __name__ == "__main__":
+	from django.core.management import execute_from_command_line
+	execute_from_command_line(sys.argv)
