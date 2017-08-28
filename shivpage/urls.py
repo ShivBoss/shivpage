bf4e4abb910d3e6bbd265025ab852850c4f7cefc
Shiv Keshri <shivprakashkeshri@gmail.com>
2017-08-28 22:29:12 +0530

    url0

--- shivpage/urls.py
+++ shivpage/urls.py
@@ -26,13 +26,13 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
-	urlpatterns += [
-		url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
-		]
-
-
 # if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +\
-#                     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+# 	urlpatterns += [
+# 		url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+# 		]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +\
+                    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
